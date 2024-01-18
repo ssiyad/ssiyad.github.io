d4561a5cea04830e0ddfaeb41ebe82c4c4fe40a5
Sabu Siyad <hello@ssiyad.com>
2024-01-18 17:04:19 +0530

    chore: add head and seo meta

--- app.vue
+++ app.vue
@@ -3,3 +3,16 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Sabu Siyad',
+});
+useSeoMeta({
+  title: 'Sabu Siyad',
+  ogTitle: 'Sabu Siyad',
+  ogDescription: 'I love to code, build and break',
+  ogImage: '/images/cover-image.png',
+  ogUrl: 'https://ssiyad.com',
+});
+</script>
