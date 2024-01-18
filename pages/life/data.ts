8a780590c5fff50e6bfde6bd051f1f1a748961e9
Sabu Siyad <hello@ssiyad.com>
2024-01-19 01:59:57 +0530

    chore: remove life

--- pages/life/data.ts
+++
@@ -1,8 +0,0 @@
-import { ref } from 'vue';
-
-export const columns = ref(20);
-export const rows = ref(20);
-export const cells = computed(() => columns.value * rows.value);
-export const census = ref(new Array(cells.value).fill(false));
-export const inProgress = ref(false);
-export const isPaused = ref(false);
