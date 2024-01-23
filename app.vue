4f98ffb40ee2016656c16a66f09d1dfda5cc0242
Sabu Siyad <hello@ssiyad.com>
2024-01-24 00:39:40 +0530

    chore: remove unwanted seo meta

--- app.vue
+++ app.vue
@@ -12,8 +12,6 @@ useFavicon(() =>
 );
 useSeoMeta({
   ogTitle: 'Sabu Siyad',
-  ogDescription: 'I love to code, build and break',
-  ogImage: '/images/cover-image.png',
   ogUrl: 'https://ssiyad.com',
   titleTemplate: '%s | Sabu Siyad',
 });
