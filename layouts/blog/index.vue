5fd580506ffeb422567b2ee63ceb9ae842f1e300
Sabu Siyad <hello@ssiyad.com>
2024-01-23 14:10:52 +0530

    chore: remove footer

--- layouts/blog/index.vue
+++ layouts/blog/index.vue
@@ -4,11 +4,9 @@
     <div class="grow p-4">
       <slot />
     </div>
-    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from './Header.vue';
-import Footer from './Footer.vue';
 </script>
