79abda4beca53245b607243b86aa65f9a3b6c2c3
Sabu Siyad <hello@ssiyad.com>
2024-01-23 14:06:58 +0530

    feat(ui): header style change

--- layouts/blog/Header.vue
+++ layouts/blog/Header.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="bg-blue-50 p-4">
-    <div class="m-auto flex max-w-prose items-center gap-1">
-      <a class="flex items-center gap-1" href="/">
-        <Icon name="uil:home-alt" />
-        <span>Home</span>
-      </a>
-      <Icon name="lucide:dot" />
-      <a class="flex items-center gap-1" href="/blog">
-        <Icon name="uil:book-open" />
-        <span>Blog</span>
-      </a>
+  <div class="px-8 py-4">
+    <div class="flex items-center justify-between">
+      <a href="/" class="font-mono text-xl font-bold italic">&</a>
+      <div class="flex items-center gap-1">
+        <div
+          class="flex items-center gap-1"
+          v-for="(link, idx) in links"
+          :key="link.url"
+        >
+          <a :href="link.url">
+            <Icon :name="link.icon" />
+          </a>
+          <Icon name="lucide:dot" v-if="idx + 1 < links.length" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import links from '@/resources/links';
+</script>
