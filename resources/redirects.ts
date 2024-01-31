dc1a8fbce34c150dc99c948252bea83d53d188c7
Sabu Siyad <hello@ssiyad.com>
2024-01-31 16:53:07 +0530

    chore: add resume

--- resources/redirects.ts
+++ resources/redirects.ts
@@ -1,5 +1,5 @@
 export default <{ [key: string]: string }>{
-  '/music':
-    'https://open.spotify.com/playlist/5ONWkSGfSjWjR0OAAotkKg?si=a52dbd96e0e04878&nd=1&dlsi=6e6410d820d2401b',
   '/meet': 'https://meet.google.com/cxg-nvum-xxf',
+  '/music': 'https://open.spotify.com/playlist/5ONWkSGfSjWjR0OAAotkKg',
+  '/resume': '/docs/resume.pdf',
 };
