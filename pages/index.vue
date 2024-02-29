ad67274f550adac1b62914dc804f6efddc2bad42
Sabu Siyad <hello@ssiyad.com>
2024-02-29 14:41:53 +0530

    chore(prettier): use `prettier-plugin-merge`

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <ContentDoc v-slot="{ doc }">
       <main class="m-4">
         <ContentRenderer
-          class="prose prose-slate mx-auto prose-headings:text-4xl prose-a:prose-headings:font-semibold prose-a:prose-headings:no-underline prose-hr:mx-auto prose-hr:w-1/3"
+          class="prose prose-slate mx-auto prose-headings:text-4xl
+            prose-a:prose-headings:font-semibold prose-a:prose-headings:no-underline
+            prose-hr:mx-auto prose-hr:w-1/3"
           :value="doc"
         />
       </main>
