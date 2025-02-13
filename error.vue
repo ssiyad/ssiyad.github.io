160069765cce7bcec7d4c1c147556129db1c9043
Sabu Siyad <hello@ssiyad.com>
2025-02-14 02:07:02 +0530

    chore: update error page

--- error.vue
+++ error.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="prose max-w-none p-2">
-    <div>{{ error.statusCode }} {{ error.message }}</div>
-    <div v-if="error.stack" v-html="error.stack" />
-    <a href="/">Go back home</a>
+  <div class="size-screen flex items-center justify-center">
+    <div class="prose-custom">
+      <div class="font-medium">{{ error.statusCode }} {{ error.message }}</div>
+      <div v-if="error.stack" v-html="error.stack" />
+      <a href="/">Go back home</a>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { NuxtError } from '#app';
 
-const props = defineProps({
+defineProps({
   error: {
     type: Object as () => NuxtError,
     required: true,
