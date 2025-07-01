ccacd8deac0f35c187ee97bf75f1b328b1445778
Sabu Siyad <hello@ssiyad.com>
2025-07-01 14:46:06 +0530

    chore: git init

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.html", "./content/**/*.{html,md}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
